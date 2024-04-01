a79a0357f61e6aac86781897382b7688e1ea3cd6
Oliveira-Renato <renato.pritf@gmail.com>
2024-04-01 04:12:20 -0300

    comment: details

--- web-SGS/src/components/Details/Details.jsx
+++ web-SGS/src/components/Details/Details.jsx
@@ -1,42 +1,46 @@
-import API from '../../services/API'
 import { Link, useParams } from 'react-router-dom'
 import { useEffect, useState } from 'react'
+import API from '../../services/API'
 import "./styles.css"
 
-export default function Details () {
-  const { id } = useParams();
-  const [data, setData] = useState({});
-  const [plataformas, setPlataformas] = useState([]);
+export default function Details() {
+  const { id } = useParams(); // Obtém o parâmetro 'id' da URL
+  const [data, setData] = useState({}); // Estado para armazenar os dados do jogo
+  const [plataformas, setPlataformas] = useState([]); // Estado para armazenar as plataformas de venda do jogo
 
+  // Efeito para buscar os dados do jogo e as plataformas de venda ao montar o componente
   useEffect(() => {
     handleFetchData();
   }, []);
 
+  // Função para buscar os dados do jogo e as plataformas de venda da API
   const handleFetchData = async () => {
     try {
+      // Faz uma requisição GET para a rota 'games/:id' para obter os detalhes do jogo específico
       const response = await API.get(`games/${id}`);
-      setData(response.data);
+      setData(response.data); // Atualiza o estado com os dados do jogo recebidos da API
+      // Verifica se há plataformas de venda disponíveis e as define no estado correspondente
       if (response.data.lojas) {
         setPlataformas(response.data.lojas.split("/"));
       }
     } catch (error) {
-      console.error("Erro ao busca-100r data:", error);
+      console.error("Erro ao buscar dados:", error);
     }
   };
 
   return (
-    <div className="mt-24 main_bg  overflow-hidden">
+    <div className="mt-24 main_bg overflow-hidden">
       <div className="h-[300px] overflow-hidden relative z-10">
-        {/* Imagem com opacidade por trás */}
+        {/* Imagem de fundo com opacidade */}
         <img src={data.imagem} alt="jogo" className="w-full h-full object-cover opacity-8" />
         {/* Imagem principal */}
         <img src={data.imagem} alt="jogo2" className="absolute top-0 object-contain" />
       </div>
-      {/* Info */}
+      {/* Informações do jogo */}
       <main className='paddingX main_bg'>
         <div className={`-mt-24 pb-14 paddingX relative second_bg`}>
           <div className='flex flex-wrap justify-around'>
-            {/* card 1 */}
+            {/* Card 1 - Detalhes do jogo */}
             <div className="third_bg p-10 rounded-b-3xl w-full md:w-[50%] h-fit z-20">
               <p className="text-white font-bold text-[40px]">{data.nome}</p>
               <div className="mt-1">
@@ -47,20 +51,20 @@ export default function Details () {
                   </div>
                 </div>
               </div>
-              {/* Botão comprar */}
+              {/* Botão de compra */}
               <button className="red_gradient w-full text-white px-4 py-2 my-2 rounded-md buy_button">Comprar</button>
             </div>
 
-            {/* card 2 - descrição */}
+            {/* Card 2 - Descrição do jogo */}
             <div className='mt-10 md:mt-32 w-full md:w-[50%] lg:w-[40%]'>
               <p className="text-white">{data.descricao}</p>
 
               <div className='mt-6'>
                 <p className='text-gray-400 text-sm mb-2'>Além de comprar aqui, você também pode encontrar em alguma de nossas lojas físicas:</p>
+                {/* Renderiza as plataformas de venda do jogo ou uma mensagem se não houver nenhuma */}
                 {plataformas.length > 0 ? (
                   plataformas.map((item, index) => (
-                    //nome da loja aqui
-                    <Link key={index} to={`/maps/${item}`}>
+                    <Link key={index} to={`/maps/${item}`}> {/* Link para as informações da loja no mapa */}
                       <p className='list_item'> 🏬 {item}</p>
                     </Link>
                   ))
@@ -73,5 +77,5 @@ export default function Details () {
         </div>
       </main>
     </div>
-  )
+  );
 }

f14726de1d56e4b518b6d000a7580102857d16f4
Oliveira-Renato <renato.pritf@gmail.com>
2024-03-31 21:40:30 -0300

    feature: description done

--- web-SGS/src/components/Details/Details.jsx
+++ web-SGS/src/components/Details/Details.jsx
@@ -6,7 +6,8 @@ import { useEffect, useState } from 'react'
 
 const Details = () => {
   const { id } = useParams();
-  const [data, setData] = useState([]);
+  const [data, setData] = useState({});
+  const [plataformas, setPlataformas] = useState([]);
 
   useEffect(() => {
 
@@ -15,45 +16,49 @@ const Details = () => {
 
   const handleFetchData = async () => {
     try {
-      const response = await API.get(`/games/${id}`);
-      setData(response.data)
+      const response = await API.get(`games/${id}`);
+      setData(response.data);
+      if (response.data.lojas) {
+        setPlataformas(response.data.lojas.split("/"));
+      }
     } catch (error) {
       console.error("Erro ao buscar data:", error);
     }
-  }
+  };
 
   return (
-    <section className='relative w-full h-screen flex justify-start items-center mx-auto'>
-      <div className="mt-12 bg-black-100 rounded-[20px]">
-        <div className={`${styles.padding} bg-tertiary rounded-2xl min-h-[300px]`}>
-          <div>
-            <img src={data.imagem} alt={"jogo"} className="w-full" />
-          </div>
-        </div>
-        {/* Description */}
-        <div className={`-mt-20 pb-14 ${styles.paddingX} flex flex-wrap gap-7`}>
-          <div className="bg-black-200 p-10 rounded-3xl xs:w-[320px] w-full">
-            <p className="text-white font-bold text-[48px]">{data.nome}</p>
-            
+    <div className="mt-12 bg-black-100 rounded-[20px] overflow-hidden">
+      <div className="bg-tertiary h-[300px] overflow-hidden relative">
+        {/* Imagem com opacidade por trás */}
+        <img src={data.imagem} alt="jogo" className="w-full h-full object-cover" />
+        {/* Imagem principal */}
+        <img src={data.imagem} alt="jogo2" className="absolute top-0 object-contain" />
+      </div>
+      {/* Info */}
+      <div className={`-mt-20 pb-14 ${styles.paddingX} relative`}>
+        <div className='flex flex-wrap justify-around'>
+          {/* card 1 */}
+          <div className="bg-black-200 p-10 rounded-b-3xl w-full md:w-[50%] max-h-[280px] max-h-[380px]">
+            <p className="text-white font-bold text-[40px]">{data.nome}</p>
             <div className="mt-1">
               <p className="text-white tracking-wider text-[18px]">{data.plataformas}</p>
-
               <div className="mt-7 flex justify-between items-center gap-1">
                 <div className="flex-1 flex flex-col">
-                  <p className="text-white font-medium text-[16px]">
-                    <span className="blue-text-gradient">R$</span> {data.preco}
-                  </p>
-                  <p className="mt-1 text-secondary text-[12px]">
-                    não sei of aaasss
-                  </p>
+                  <p className="text-white font-medium text-[16px]">R${data.preco}</p>
                 </div>
               </div>
             </div>
+            {/* Botão comprar */}
+            <button className="bg-primary w-full text-white px-4 py-2 my-2 rounded-md hover:bg-primary-dark">Comprar</button>
           </div>
-          
+
+          {/* card 2 - descrição */}
+          <div className='mt-10 md:mt-32 w-full md:w-[50%] lg:w-[40%]'>
+            <p className="text-white">{data.descricao}</p>
+          </div>
         </div>
       </div>
-    </section>
+    </div>
   )
 }
 

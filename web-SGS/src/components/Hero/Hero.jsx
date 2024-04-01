af942fd544ec7d7cf70380d95be67a1207c7705f
Oliveira-Renato <renato.pritf@gmail.com>
2024-04-01 04:06:37 -0300

    comment: hero

--- web-SGS/src/components/Hero/Hero.jsx
+++ web-SGS/src/components/Hero/Hero.jsx
@@ -1,20 +1,26 @@
-export default function Hero () {
+import React from 'react';
+
+export default function Hero() {
   return (
+    // Seção da tela principal
     <section className='relative w-full h-screen flex justify-start items-center mx-auto'>
       <div className={`paddingX max-w-7xl mx-auto flex flex-col gap-5`}>
-        {/* info */}
+        {/* Informações da tela principal */}
         <div>
+          {/* Título da tela principal */}
           <h1 className={`heroHeadText text-white gray-gradient px-12 rounded-md`}>
             Smart Games {' '}
-            <div className=" relative inline-block">
+            {/* Estilização do texto 'Store' com gradiente vermelho */}
+            <div className="relative inline-block">
               <span className="red_200 relative z-10">Store</span>
             </div>
           </h1>
-          <p className={`heroSubText text-center text-white  self-center rounded-md`}>
+          {/* Subtítulo da tela principal */}
+          <p className={`heroSubText text-center text-white self-center rounded-md`}>
             Explore, compre, divirta-se!
           </p>
         </div>
       </div>
     </section>
-  )
+  );
 }
